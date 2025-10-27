4b5d20fcadb95570f348266e8d311ab6cb259228
Alessya Tkachenko <tkachenko.alessya@gmail.com>
2025-10-27 19:42:37 +0500

    feat: add containsSubstring function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -266,8 +266,8 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  return minutes + seconds;
 }
 
 /**
@@ -280,8 +280,8 @@ function formatTime(/* minutes, seconds */) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str;
 }
 
 /**
@@ -311,8 +311,8 @@ function orderAlphabetically(/* str */) {
  *   containsSubstring('JavaScript is Fun', 'Python') => false
  *   containsSubstring('12345', '34') => true
  */
-function containsSubstring(/* str, substring */) {
-  throw new Error('Not implemented');
+function containsSubstring(str, substring) {
+  return str.includes(substring);
 }
 
 /**
